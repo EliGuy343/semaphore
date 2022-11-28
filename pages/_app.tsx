2bb1a865f6a0d588d8fb115dcd734d3bfdbf6678
Eli Guy <eliguy343@gmail.com>
2022-11-28 06:54:12 +0200

    next-auth setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
-const Home: NextPage = () => {
+import {getProviders, getSession, useSession } from 'next-auth/react';
+
+const Home = ({trendingResults, followResults, providers}) => {
+  const {data: session } = useSession();
+
+  // if(!session) return <Login providers={providers}>
   return (
     <div className=''>
       <Head>
@@ -16,6 +21,17 @@ const Home: NextPage = () => {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const trendingResult = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  );
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const sessions = await getSession(context);
 }
 
 export default Home
