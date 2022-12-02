e0919fad06194e1fa6d51d6ccbc400e408a94ad6
Eli Guy <eliguy343@gmail.com>
2022-12-02 13:52:48 +0200

    Post page design

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -13,7 +13,7 @@ import Modal from "../components/Modal";
 import Sidebar from "../components/Sidebar";
 import Post from "../components/Post";
 import {db } from '../firebase';
-import { ArrowDownLeftIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import Head from "next/head";
 import Login from "../components/Login";
 
@@ -41,6 +41,40 @@ const PostPage = ({trendingResults, followResults, providers}) => {
         className='bg-black min-h-screen flex justify-start max-w-[1500px]
           mx-auto'
       >
+        <div
+          className="flex-grow border-l border-r border-gray-700
+            max-w-2xl sm:ml-[73px] xl:ml-[370px]"
+        >
+          <div
+            className="flex items-center px-1.5 py-2 border-b
+              border-gray-700 text-[#d9d9d9] font-semibold text-xl
+              gap-x-4 sticky top-0 z-50 bg-black"
+          >
+            <div
+              className="hoverAnimation w-9 h-9 flex items-center
+                justify-center xl:px-0"
+              onClick={() =>  router.push("/")}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Back To Feed
+          </div>
+          {post ?
+            (
+              <Post id={id} post={post} postPage/>
+            )
+            :(
+            <div
+              className=" flex text-white justify-center text-center
+                px-1.5 py-2"
+            >
+              <h2 className="font-bold text-[20px]">
+                We are sorry, but the post doesn't exist anymore.
+              </h2>
+            </div>
+            )
+          }
+        </div>
         <Sidebar/>
         {isOpen && <Modal/>}
       </main>
