7ca53c076f88a98bb1e39ce0f3f867939ccf9c15
Eli Guy <eliguy343@gmail.com>
2022-12-04 00:57:02 +0200

    fixed jwt error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
+
 export default NextAuth({
+  secret: process.env.JWT_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -16,5 +18,4 @@ export default NextAuth({
       return session;
     }
   },
-  secret: process.env.JWT_SECRET
 });
