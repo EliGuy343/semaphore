71998f9fbdfbd4bdefb0fb5f90dcbbce9b664e48
Eli Guy <eliguy343@gmail.com>
2022-12-20 08:06:11 +0200

    Search result display

--- pages/search/[word].jsx
+++ pages/search/[word].jsx
@@ -23,15 +23,10 @@ import Widgets from '../../components/Widgets';
 import PhotoModal from "../../components/PhotoModal";
 
 
-const InitialbufferLimit = 100;
-const initialFeedLimit = 50;
-
 
 const SearchPage = ({}) => {
   const router = useRouter();
   const {data: session} = useSession();
-  const [bufferLimit, setBufferLimit ] = useState(InitialbufferLimit);
-  const [feedLimit, setFeedLimit]  = useState(initialFeedLimit);
   const [postBuffer, setPostBuffer] = useState([]);
   const [posts, setPosts] = useState([]);
   const [changed, setChanged] = useState(false);
@@ -49,7 +44,6 @@ const SearchPage = ({}) => {
     getDocs(query(
       collection(db, 'posts'),
       orderBy('timestamp', 'desc'),
-      limit(bufferLimit),
     )).then(result => {
       setPostBuffer(result.docs)
       console.log(postBuffer)
@@ -68,7 +62,42 @@ const SearchPage = ({}) => {
  }, [postBuffer])
 
  return (<div>
-  test
+   <Head>
+      <title>Semaphore</title>
+      <link rel="icon" href="/signal.ico" />
+    </Head>
+    <main
+        className='bg-black min-h-screen flex justify-start max-w-[1500px]
+          mx-auto'
+      >
+        <div
+          className="flex-grow border-l border-r border-gray-700
+            max-w-2xl sm:ml-[73px] xl:ml-[370px]"
+        >
+          <div
+            className="flex items-center px-1.5 py-2 border-b
+              border-gray-700 text-[#d9d9d9] font-semibold text-xl
+              gap-x-4 sticky top-0 z-50 bg-black"
+          >
+            <div
+              className="hoverAnimation w-9 h-9 flex items-center
+                justify-center xl:px-0"
+              onClick={() =>  router.push("/")}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Back To Feed
+          </div>
+          {posts?.map(post => (
+            <Post
+              key={post.id}
+              id={post.id}
+              post={post.data()}
+              setChanged={setChanged}
+            />
+        ))}
+        </div>
+    </main>
  </div>)
 
 }
