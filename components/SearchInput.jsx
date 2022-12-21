74bc324abdcdaf0318ce2783d6b9ad7d7bba4e70
Eli Guy <eliguy343@gmail.com>
2022-12-21 14:24:50 +0200

    Search Input design

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -23,7 +23,8 @@ const SearchInput = () => {
           />
         </div>
       </div>
-      <div className='flex flex-row space-x-2'>
+      <div className='mt-5'>
+        <h1 className='pl-2 text-white text-[15px] font-bold'>Username:</h1>
         <div className="w-[50%] divide-y-2 divide-gray-700">
           <div
             className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
@@ -41,41 +42,55 @@ const SearchInput = () => {
             />
           </div>
         </div>
-        <div className="w-[25%] divide-y-2 divide-gray-700">
-          <div
-            className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
-          >
-            <input
-              className="bg-transparent placeholder-gray-500 outline-none
-                text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
-                w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
-                focus:shadow-lg"
-              placeholder="from"
-              // onChange={(e) => setSearchInput(e.target.value)}
-              // onKeyDown={ (e) => {
-              //   if(e.key == 'Enter') goToSearch();
-              // }}
-            />
+      </div>
+      <div className='mt-5'>
+        <h1 className='pl-2 text-white text-[15px] font-bold'>Date:</h1>
+        <div className='flex flex-row space-x-4'>
+          <div className="w-[50%] divide-y-2 divide-gray-700">
+            <div
+              className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
+            >
+              <input
+                className="bg-transparent placeholder-gray-500 outline-none
+                  text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
+                  w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
+                  focus:shadow-lg"
+                style={{
+                  colorScheme:'dark'
+                }}
+                placeholder="from"
+                type='date'
+                // onChange={(e) => setSearchInput(e.target.value)}
+                // onKeyDown={ (e) => {
+                //   if(e.key == 'Enter') goToSearch();
+                // }}
+              />
+            </div>
           </div>
-        </div>
-        <div className="w-[25%] divide-y-2 divide-gray-700">
-          <div
-            className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
-          >
-            <input
-              className="bg-transparent placeholder-gray-500 outline-none
-                text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
-                w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
-                focus:shadow-lg"
-              placeholder="To"
-              // onChange={(e) => setSearchInput(e.target.value)}
-              // onKeyDown={ (e) => {
-              //   if(e.key == 'Enter') goToSearch();
-              // }}
-            />
+          <div className="w-[50%] divide-y-2 divide-gray-700">
+            <div
+              className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
+            >
+              <input
+                className="bg-transparent placeholder-gray-500 outline-none
+                  text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
+                  w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
+                  focus:shadow-lg"
+                style={{
+                  colorScheme:'dark'
+                }}
+                type='date'
+                placeholder="To"
+                // onChange={(e) => setSearchInput(e.target.value)}
+                // onKeyDown={ (e) => {
+                //   if(e.key == 'Enter') goToSearch();
+                // }}
+              />
+            </div>
           </div>
         </div>
       </div>
+      
       <div className='flex flex-row items-center justify-center'>
         <button
           className="bg-[#1d9bf0] text-white rounded-full px-4 py-2 mt-5
