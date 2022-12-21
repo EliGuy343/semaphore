ea64a9caa05f011bab866f2adda946bec3fb59f1
Eli Guy <eliguy343@gmail.com>
2022-12-21 14:39:37 +0200

    Search Input design done for nw

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -15,7 +15,7 @@ const SearchInput = () => {
               text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
               w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
               focus:shadow-lg"
-            placeholder="Search for what you need"
+            placeholder="Search for..."
             // onChange={(e) => setSearchInput(e.target.value)}
             // onKeyDown={ (e) => {
             //   if(e.key == 'Enter') goToSearch();
@@ -23,11 +23,11 @@ const SearchInput = () => {
           />
         </div>
       </div>
-      <div className='mt-5'>
-        <h1 className='pl-2 text-white text-[15px] font-bold'>Username:</h1>
+      <div className='mt-5 flex flex-col justify-center items-center'>
+        <h1 className='pl-2 text-white text-[15px] font-bold'>From Username:</h1>
         <div className="w-[50%] divide-y-2 divide-gray-700">
           <div
-            className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
+            className="flex items-center bg-[#202327] p-4 rounded-full relative mt-5 border"
           >
             <input
               className="bg-transparent placeholder-gray-500 outline-none
@@ -35,6 +35,9 @@ const SearchInput = () => {
                 w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
                 focus:shadow-lg"
               placeholder="@username"
+              style={{
+                colorScheme:'dark'
+              }}
               // onChange={(e) => setSearchInput(e.target.value)}
               // onKeyDown={ (e) => {
               //   if(e.key == 'Enter') goToSearch();
@@ -43,57 +46,57 @@ const SearchInput = () => {
           </div>
         </div>
       </div>
-      <div className='mt-5'>
-        <h1 className='pl-2 text-white text-[15px] font-bold'>Date:</h1>
-        <div className='flex flex-row space-x-4'>
-          <div className="w-[50%] divide-y-2 divide-gray-700">
-            <div
-              className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
-            >
-              <input
-                className="bg-transparent placeholder-gray-500 outline-none
-                  text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
-                  w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
-                  focus:shadow-lg"
-                style={{
-                  colorScheme:'dark'
-                }}
-                placeholder="from"
-                type='date'
-                // onChange={(e) => setSearchInput(e.target.value)}
-                // onKeyDown={ (e) => {
-                //   if(e.key == 'Enter') goToSearch();
-                // }}
-              />
-            </div>
-          </div>
-          <div className="w-[50%] divide-y-2 divide-gray-700">
-            <div
-              className="flex items-center bg-[#202327] p-6 rounded-full relative mt-5 border"
-            >
-              <input
-                className="bg-transparent placeholder-gray-500 outline-none
-                  text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
-                  w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
-                  focus:shadow-lg"
-                style={{
-                  colorScheme:'dark'
-                }}
-                type='date'
-                placeholder="To"
-                // onChange={(e) => setSearchInput(e.target.value)}
-                // onKeyDown={ (e) => {
-                //   if(e.key == 'Enter') goToSearch();
-                // }}
-              />
-            </div>
+      <div className='mt-5 flex flex-col justify-center items-center'>
+        <h1 className='pl-2 text-white text-[15px] font-bold'>From Date:</h1>
+        <div className="w-[50%] divide-y-2 divide-gray-700">
+          <div
+            className="flex items-center bg-[#202327] p-4 rounded-full relative mt-5 border"
+          >
+            <input
+              className="bg-transparent placeholder-gray-500 outline-none
+                text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent
+                w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
+                focus:shadow-lg"
+              placeholder="from"
+              type="date"
+              style={{
+                colorScheme:'dark'
+              }}
+              // onChange={(e) => setSearchInput(e.target.value)}
+              // onKeyDown={ (e) => {
+              //   if(e.key == 'Enter') goToSearch();
+              // }}
+            />
           </div>
         </div>
       </div>
-      
+      <div className='mt-3 flex flex-col justify-center items-center'>
+        <h1 className='pl-2 text-white text-[15px] font-bold'>To Date:</h1>
+        <div className="w-[50%] divide-y-2 divide-gray-700">
+          <div
+            className="flex items-center bg-[#202327] p-4 rounded-full relative mt-5 border"
+          >
+            <input
+              className="bg-transparent placeholder-gray-500 outline-none
+                text-[#d9d9d9] absolute inset-0 pl-8 border border-transparent
+                w-full focus:border-[#1d9bf0] rounded-full focus:bg-black
+                focus:shadow-lg"
+              placeholder="To"
+              type="date"
+              style={{
+                colorScheme:'dark'
+              }}
+              // onChange={(e) => setSearchInput(e.target.value)}
+              // onKeyDown={ (e) => {
+              //   if(e.key == 'Enter') goToSearch();
+              // }}
+            />
+          </div>
+        </div>
+      </div>
       <div className='flex flex-row items-center justify-center'>
         <button
-          className="bg-[#1d9bf0] text-white rounded-full px-4 py-2 mt-5
+          className="bg-[#1d9bf0] text-white rounded-full px-2 py-1 mt-7
             font-bold shadow-md hover:bg-[#1a8cd8] w-[40%]
             disabled:hover:bg-[#1d9bf0] disabled:opacity-40
             disabled:cursor-deafult"
