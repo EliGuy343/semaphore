b966089dc1d1d9e630455670f096bcba85fb5003
Eli Guy <eliguy343@gmail.com>
2022-12-25 10:18:47 +0200

    user info to database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,33 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
+import {
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  getDoc,
+  addDoc,
+  doc,
+  setDoc
+} from "firebase/firestore";
+import { db } from '../../../firebase';
 
+async function CreateGetUser(email, userImage) {
+
+  const userRef = doc(db, "posts", email);
+  const userDoc = await getDoc(userRef);
+
+  if(!userDoc.exists()) {
+    await setDoc(doc(db, 'users', email), {
+      email: email,
+      image: userImage
+    })
+  }
+
+
+}
 
 export default NextAuth({
   secret: process.env.JWT_SECRET,
@@ -15,6 +42,7 @@ export default NextAuth({
       session.user.tag = session.user.name
         .split(' ').join('').toLocaleLowerCase();
       session.user.uid = token.sub;
+      CreateGetUser(session.user.email, session.user.image)
       return session;
     }
   },
