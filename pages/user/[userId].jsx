76cecb1041b0c7a272d19b041deece6324c3e530
Eli Guy <eliguy343@gmail.com>
2022-12-27 12:36:32 +0200

    sepreated User to it's own component

--- pages/user/[userId].jsx
+++ pages/user/[userId].jsx
@@ -23,6 +23,7 @@ import Post from '../../components/Post';
 import AdvancedSearchModal from '../../components/AdvancedSearchModal';
 import Modal from '../../components/Modal';
 import PhotoModal from '../../components/PhotoModal';
+import User from "../../components/User";
 
 const initalPostsLimit = 5;
 
@@ -173,34 +174,7 @@ const UserPage = ({trendingResults, followResults, providers}) => {
           </div>
           Back To Feed
         </div>
-        {!notFound && !loading && <div className="border-b border-gray-700 p-5 flex space-x-3">
-          <div className="flex items-center space-x-4">
-            <img
-              src={user.image}
-              alt="profile pic"
-              className="h-[150px] w-[150px] rounded-full"
-            />
-            <div className="flex flex-col space-x-2 space-y-2 items-start justify-between p-3">
-              <div>
-                <h2 className="text-white font-bold">{user.name}</h2>
-                <h3 className="text-gray-500">@{user.tag}</h3>
-              </div>
-              <h3 className="text-white">
-                {user.bio}
-              </h3>
-              <div className="flex space-x-2">
-                <div className="flex space-x-1 items-center justify-center">
-                  < MapPinIcon className="text-gray-500 h-[22px]"/>
-                  <h2 className="text-gray-500">
-                    {user.location ? (`${user.location}`)
-                      :"Location not provided"
-                    }
-                  </h2>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>}
+        {!notFound && !loading && <User userPage={true} user={user}/>}
         {newerPostsNotification &&
           <div
             className="p-3 flex border-b border-gray-700
