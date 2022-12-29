b97da6f17373f1df37a8b794e20bc4469d54c6e3
Eli Guy <eliguy343@gmail.com>
2022-12-29 21:09:42 +0200

    search users data fetching + design

--- pages/search/users.jsx
+++ pages/search/users.jsx
@@ -1,3 +1,14 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Modal from "../../components/Modal";
+import PhotoModal from "../../components/PhotoModal";
+import Sidebar from "../../components/Sidebar";
+import User from "../../components/User";
+import Widgets from "../../components/Widgets";
 import {
   collection,
   doc,
@@ -8,37 +19,11 @@ import {
   query,
   where
 } from "@firebase/firestore";
-import {getProviders, getSession, useSession } from 'next-auth/react';
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import Modal from "../../components/Modal"
-import Sidebar from "../../components/Sidebar";
-import Post from "../../components/Post";
-import {db } from '../../firebase';
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
-import Head from "next/head";
-import Login from "../../components/Login";
-import Widgets from '../../components/Widgets';
-import PhotoModal from "../../components/PhotoModal";
-import SearchInput from "../../components/SearchInput";
+import { db } from "../../firebase";
 
+const SearchUsersPage = ({trendingResults, providers}) => {
 
-
-const SearchPage = ({trendingResults, followResults, providers}) => {
   const router = useRouter();
-  const { word, endDate, startDate, user } = router.query;
-  const {data: session} = useSession();
-  const [postBuffer, setPostBuffer] = useState([]);
-  const [posts, setPosts] = useState([]);
-  const [advancedSearch, setAdvancedSearch] = useState({
-    startDate: startDate,
-    endDate: endDate,
-    user:user,
-    word: word
-  });
-
-  const [changed, setChanged] = useState(false);
 
   const { isOpen } = useSelector((state) => {
     return state.modalState
@@ -47,51 +32,42 @@ const SearchPage = ({trendingResults, followResults, providers}) => {
     return state.photoModalState.isOpen
   }));
 
+  const { tag, user } = router.query;
+  const {data: session} = useSession();
+  const [userBuffer, setUserBuffer] = useState([]);
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     let searchQuery = query(
-      collection(db, 'posts'),
-      orderBy('timestamp', 'desc')
+      collection(db, 'users'),
     )
 
     getDocs(searchQuery).then(result => {
-      setPostBuffer(result.docs)
+      console.log(result.docs)
+      setUserBuffer(result.docs)
     });
-  }, [advancedSearch])
-
-
+  }, [])
 
   useEffect(() => {
     const newPosts = [];
-    for(let i = 0; i < postBuffer.length; i++) {
-      if(advancedSearch.word) {
-        if(!postBuffer[i].data().text.includes(advancedSearch.word))
+    for(let i = 0; i < userBuffer.length; i++) {
+      if(user) {
+        if(!userBuffer[i].data().name.includes(user))
           continue;
       }
-
-    if(advancedSearch.startDate) {
-      const startDate = Date.parse(advancedSearch.startDate);
-      const timestamp = postBuffer[i].data().timestamp.seconds * 1000;
-      if(timestamp <= startDate) continue;
+      if(tag) {
+        if(!userBuffer[i].data().tag.includes(tag))
+        continue;
+      }
+      newPosts.push(userBuffer[i]);
     }
+    setUsers(newPosts);
+  }, [userBuffer])
 
-    if(advancedSearch.endDate) {
-      const endDate = Date.parse(advancedSearch.endDate);
-      const timestamp = postBuffer[i].data().timestamp.seconds * 1000;
-      if(timestamp >= endDate) continue;
-    }
+  if(!session) return <Login providers={providers}/>
 
-    if(advancedSearch.user) {
-      if(advancedSearch.user !== postBuffer[i].data().tag) continue;
-    }
-    newPosts.push(postBuffer[i]);
-    }
-    setPosts(newPosts);
-  }, [postBuffer])
-
- if(!session) return <Login providers={providers}/>
-
- return (<div>
-   <Head>
+  return (<div>
+    <Head>
       <title>Semaphore</title>
       <link rel="icon" href="/signal.ico" />
     </Head>
@@ -117,14 +93,11 @@ const SearchPage = ({trendingResults, followResults, providers}) => {
             </div>
             Back To Feed
           </div>
-          {posts?.map(post => (
-            <Post
-              key={post.id}
-              id={post.id}
-              post={post.data()}
-              setChanged={setChanged}
-            />
-        ))}
+          <div className="flex flex-col">
+            {users.map((user) => {
+              return <User key={user.id} user={user.data()}/>
+            })}
+          </div>
         </div>
         <Sidebar/>
         <Widgets
@@ -134,7 +107,7 @@ const SearchPage = ({trendingResults, followResults, providers}) => {
         {isPhotoModalOpen && <PhotoModal/>}
         {isOpen && <Modal/>}
     </main>
- </div>)
+  </div>)
 }
 
 export async function getServerSideProps(context) {
@@ -167,6 +140,4 @@ export async function getServerSideProps(context) {
     }
   }
 }
-
-
-export default SearchPage;
+export default SearchUsersPage;
